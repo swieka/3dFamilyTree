e873e11212247ace7535d02e4d052cd9330b98e0
swieka <swieczkowski@gmail.com>
2019-05-07 21:40:47 +0200

    Initial 3d web tree

---
+++ Web/readme.txt
@@ -0,0 +1,2 @@
+docker build -t gedcom3d .
+docker run -p 49160:8080 -d gedcom3d
---
+++ Web/server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+var path = require('path');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.get('/gedcom', (req, res) => {
+  res.sendFile(path.join(__dirname + '/gedcom.json'));
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
